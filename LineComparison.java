0bfbf46c3e60662695abea0d16de6ab422a34bfe
Sankar Vijaya Selvakumar <vs.sankar95@gmail.com>
2021-02-08 17:48:40 +0530

    [Sankar]ADDED:Prints Welcome

---
+++ LineComparison.java
@@ -0,0 +1,5 @@
+public class LineComparison {
+	public static void main(String[] args) {
+                System.out.println("***** WELCOME *****");
+        }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+## Welcome to Line Comprison Program
