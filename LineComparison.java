117443d25c809df7723a9ad8e1bf76e65284b466
Sankar Vijaya Selvakumar <vs.sankar95@gmail.com>
2021-02-08 17:54:56 +0530

    [Sankar]ADDED:Calculate the Length of a Modelled Line

--- LineComparison.java
+++ LineComparison.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
 public class LineComparison {
 	public static void main(String[] args) {
-                System.out.println("***** WELCOME *****");
+                //Creating a Scanner Obj for multiple uses
+		Scanner sc = new Scanner(System.in);
+                System.out.println("Modelling a Line based on (x,y) co-ordinates");
+		//Asking User to enter the co-ordinates
+                System.out.println("Enter (x1,y1) co-ordinates: ");
+                float x1 = sc.nextFloat(),y1 = sc.nextFloat();
+                System.out.println("Enter (x2,y2) co-ordinates: ");
+                float x2 = sc.nextFloat(),y2 = sc.nextFloat();
+		//Calculating the Modelled Line Length
+                double lengthLine1 = Math.sqrt( Math.pow((x2-x1),2) + Math.pow((y2-y1),2 ) );
+                System.out.println("Length of the modelled Line: "+lengthLine1);
         }
 }
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 ## Welcome to Line Comprison Program
+### Used Case:1
+#### Model a line based on a point consisting of (x, y) co-ordinatesusing the Cartesian system to calculate its length.
+
