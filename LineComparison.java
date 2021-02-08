beb109628cc723ab77bab41e26a86983096d31c0
Sankar Vijaya Selvakumar <vs.sankar95@gmail.com>
2021-02-08 18:02:52 +0530

    [Sankar]ADDED.Check equality for 2 modelled lines

--- LineComparison.java
+++ LineComparison.java
@@ -4,13 +4,34 @@ public class LineComparison {
                 //Creating a Scanner Obj for multiple uses
 		Scanner sc = new Scanner(System.in);
                 System.out.println("Modelling a Line based on (x,y) co-ordinates");
-		//Asking User to enter the co-ordinates
-                System.out.println("Enter (x1,y1) co-ordinates: ");
-                float x1 = sc.nextFloat(),y1 = sc.nextFloat();
-                System.out.println("Enter (x2,y2) co-ordinates: ");
-                float x2 = sc.nextFloat(),y2 = sc.nextFloat();
-		//Calculating the Modelled Line Length
-                double lengthLine1 = Math.sqrt( Math.pow((x2-x1),2) + Math.pow((y2-y1),2 ) );
-                System.out.println("Length of the modelled Line: "+lengthLine1);
+		//Asking User to enter Line-1 co-ordinates
+                System.out.println("Enter (x1,y1) co-ordinates for Line-1: ");
+                float x1_Line1 = sc.nextFloat(),y1_Line1 = sc.nextFloat();
+                System.out.println("Enter (x2,y2) co-ordinates for Line-1: ");
+                float x2_Line1 = sc.nextFloat(),y2_Line1 = sc.nextFloat();
+
+		//Asking user to enter Line-2 co-ordinates
+		System.out.println("Modelling Line-2 based on (x,y) co-ordinates");
+                System.out.println("Enter (x1,y1) co-ordinates for Line-2: ");
+                float x1_Line2 = sc.nextFloat(),y1_Line2 = sc.nextFloat();
+                System.out.println("Enter (x2,y2) co-ordinates for Line-2: ");
+                float x2_Line2 = sc.nextFloat(),y2_Line2 = sc.nextFloat();
+
+		//Calculating the Modelled Line-1 Length
+                double lengthLine1 = Math.sqrt( Math.pow((x2_Line1-x1_Line1),2) + Math.pow((y2_Line1-y1_Line1),2 ) );
+
+		//Calculating the Modelled Line-2 Length
+		double lengthLine2 = Math.sqrt( Math.pow((x2_Line2-x1_Line2),2) + Math.pow((y2_Line2-y1_Line2),2 ) );
+
+		//Checking Equality
+		if ( lengthLine1 == lengthLine2 ) {
+                        System.out.println("The two modelled lines are equal in Length.");
+                }
+                else {
+                        System.out.println("The two modelled lines are un-equal in Length.");
+                }
+		//Printing the Length of two Modelled Lines
+                System.out.println("Length of the modelled Line-1: "+lengthLine1);
+                System.out.println("Length of the modelled Line-2: "+lengthLine2);
         }
 }
--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 ## Welcome to Line Comprison Program
 ### Used Case:1
 #### Model a line based on a point consisting of (x, y) co-ordinatesusing the Cartesian system to calculate its length.
-
+### Used Case:2
+#### check equality of two lines based on the end points, to know when two lines are the equal.
